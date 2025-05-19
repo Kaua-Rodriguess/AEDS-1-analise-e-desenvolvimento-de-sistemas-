bab598fb6f47c1e446d9b035b5b4b633471efe65
kauã rodrigues <kauarodriges17@gmail.com>
2025-05-19 11:37:16 -0300

    equação do 2º grau

--- AEDS I/Aulas 3 e 4 - Estruturas Condicionais/Lista 2-condicional/Lista 02- Condicional-EX-2.c
+++ AEDS I/Aulas 3 e 4 - Estruturas Condicionais/Lista 2-condicional/Lista 02- Condicional-EX-2.c
@@ -1,9 +1,9 @@
-/*! 2. Fazer um programa para ler dois números inteiros e calcular e imprimir a diferença desses
- números. Depois, se o segundo número for diferente de zero, fazer a divisão do primeiro
+/*! 2. Fazer um programa para ler dois nÃºmeros inteiros e calcular e imprimir a diferenÃ§a desses
+ numeros. Depois, se o segundo nï¿½mero for diferente de zero, fazer a divisao do primeiro
  pelo segundo.
- 1ºler dois números inteiros
- 2ºcalcular e imprimir a diferença desses números
- 3ºse o segundo número for diferente de zero, fazer a divisão do primeiro
+ 1-ler dois nï¿½meros inteiros
+ 2-calcular e imprimir a diferenï¿½a desses nï¿½meros
+ 3-se o segundo nï¿½mero for diferente de zero, fazer a divisï¿½o do primeiro
  pelo segundo.
 
  */
--- AEDS I/Aulas 3 e 4 - Estruturas Condicionais/Lista 2-condicional/Lista 02- Condicional-EX-5.c
+++ AEDS I/Aulas 3 e 4 - Estruturas Condicionais/Lista 2-condicional/Lista 02- Condicional-EX-5.c
@@ -1,18 +1,36 @@
-/* 
+/*!
 5. Ler os coeficientes de uma equação do 2º grau, determinar e escrever suas raízes reais ou
  imaginárias.
  */
 #include <stdio.h>
+#include <math.h>
 int main()
 {
-    //equação: y=a*x^2+b*x+c
+    //!equação: y=a*x^2+b*x+c
 float a,b,c;
+
 printf("digite os valores de a b e c:");
 scanf("%f%f%f",&a,&b,&c);
 
+//!1º passo: calcular o valor de delta Δ=b*2-4ac x2 -5x +6
 
+float delta=(pow(b,2))-4*a*c;
 
-
+if(delta>0) //! analisar o valor de delta
+{
+    float x1=((-b+ sqrt(delta))/2*a);
+    float x2=((-b- sqrt(delta))/2*a);
+    printf("duas raizes reais diferente. x1=%.2f x2=%.2f",x1,x2);
+}
+else if(delta==0)
+{
+    float x1=((-b+ sqrt(delta))/2*a);
+    printf("duas raizes iguais.%.2f",x1);
+}
+else //!delta<0
+{
+    printf("duas raizes imaginarias");
+}
 
 
 
