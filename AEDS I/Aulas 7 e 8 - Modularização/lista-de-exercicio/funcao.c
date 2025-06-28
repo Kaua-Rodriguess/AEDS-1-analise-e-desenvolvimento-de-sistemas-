62e746b949a007dd9f34c1a966597f088623c7ca
kauã rodrigues <kauarodriges17@gmail.com>
2025-06-28 15:01:19 -0300

    modularização

---
+++ AEDS I/Aulas 7 e 8 - Modularização/lista-de-exercicio/funcao.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (){
+
+    apresentacao();
+
+    return 0;
+}
+
+void apresentacao(void)
+{
+    printf("ola, esse e o curso de C \n");
+    printf("esta gostando?\n");
+}
+
+
+
