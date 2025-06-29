222e178ace3aef6aaee8900638ab682e928b452b
kauã rodrigues <kauarodriges17@gmail.com>
2025-06-29 14:03:36 -0300

    modularização

--- AEDS I/Aulas 7 e 8 - Modularização/lista-de-exercicio/funcao-1.c
+++ AEDS I/Aulas 7 e 8 - Modularização/lista-de-exercicio/funcao-1.c
@@ -4,17 +4,35 @@
 
 
 int main (){
+    int v1,v2,resultado;
 
-    apresentacao();
+   printf("Digite o valor 01:\n");
+   scanf("%d",&v1);
+
+   printf("Digite o valor 02:\n");
+   scanf("%d",&v2);
+
+   resultado = multiplicacao(v1,v2);
+
+   printf("O resultado e:%d \n",resultado);
 
     return 0;
 }
 
-void apresentacao(void)
+int multiplicacao(int n1,int n2)
 {
-    printf("ola, esse e o curso de C \n");
-    printf("esta gostando?\n");
+    int resultado;
+
+    resultado= n1*n2;
+
+    return(resultado);
 }
+
+
+
+
+
+
 
 
 
