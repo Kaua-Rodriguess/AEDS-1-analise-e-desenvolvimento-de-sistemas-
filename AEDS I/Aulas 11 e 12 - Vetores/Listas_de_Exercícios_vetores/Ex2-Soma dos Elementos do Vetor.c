faf20140892aada2e33b802a60990a1ea1cc894b
kauã rodrigues <kauarodriges17@gmail.com>
2025-06-30 16:13:12 -0300

    Soma dos Elementos do Vetor

--- AEDS I/Aulas 11 e 12 - Vetores/Listas_de_Exercícios_vetores/Ex2-Soma dos Elementos do Vetor.c
+++ AEDS I/Aulas 11 e 12 - Vetores/Listas_de_Exercícios_vetores/Ex2-Soma dos Elementos do Vetor.c
@@ -1,1 +1,18 @@
+/*!
+Exercício 2: Soma dos Elementos do Vetor
 
+ Enunciado:
+ Escreva um programa em C que declare um vetor de 5 elementos inteiros.
+ O programa deve ler 5 valores do usuário, armazená-los no vetor e
+ calcular a soma de todos os elementos.
+
+ Exemplo de saída
+ Digite o valor 1: 4
+ Digite o valor 2: 8
+ Digite o valor 3: 10
+ Digite o valor 4: 2
+ Digite o valor 5: 7
+ A soma dos elementos do
+vetor é: 31
+
+*/
