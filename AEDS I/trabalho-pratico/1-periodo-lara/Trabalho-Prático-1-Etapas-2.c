927659012b277037393a6f834d930452156c67c9
kauã rodrigues <kauarodriges17@gmail.com>
2025-09-14 13:39:16 -0300

    Trabalho prático etapa 3 niveis de dificuldade

--- AEDS I/trabalho-pratico/1-periodo-lara/Trabalho-Prático-1-Etapas-2.c
+++ AEDS I/trabalho-pratico/1-periodo-lara/Trabalho-Prático-1-Etapas-2.c
@@ -11,17 +11,59 @@ int main(){
     //ETAPA 1
     srand(time(NULL));
     int numeroAleatorioAte10 = (rand() % 10)+ 1;//numeros de 0 a 10, por isso eu somo +1
-    int tentativa=0;
-    int chute;
+    int chute,tentativa=0,nivel;
+    
 
     printf("\n====Bem-vindo ao jogo de adivinhacao=======\n");
-    printf("Tente adivinhar o numero secreto entre 1 e 10\n");
-    printf("Voce tem 3 tentativas!\n");
+    printf("Tente adivinhar o numero secreto!\n");
+    printf("Primeira escolha um numero para o selecionar o nivel de dificuldade:\n");
+    printf("1-Fácil (Número entre 1 e 10, 5 tentativas)\n
+            2-Médio (Número entre 1 e 50, 7 tentativas)\n
+            3-Difícil (Número entre 1 e 100, 10 tentativas)");
+    scanf("%d",&nivel);
 
-    /*!Se errar, exibir uma mensagem indicando se o número secreto
-     é maior ou menor do que a tentativa. */
+    switch(nivel){
+        case 1:{
+            printf("\n ->NIVEL FACIL:");
+            int numeroAte10 = (rand() % 10)+1; //5 tentativas
 
-     //ETAPA 2-Número Aleatório e Múltiplas Tentativas 
+              //ETAPA 2-Número Aleatório e Múltiplas Tentativas
+     //Exibi uma mensagem indicando se o número secreto é maior ou menor do que a tentativa.*/ 
+     do{
+        printf("\nDigite um numero:");
+        scanf("%i",&chute);
+        tentativa++;
+
+         if(chute < numeroAleatorioAte10){
+            printf("Tentativa %i: %i \n",tentativa,chute);
+            printf("O numero secreto e maior!\n");
+         }
+
+         else {
+            printf("Tentativa %i: %i \n",tentativa,chute);
+            printf("O numero secreto e menor!\n");
+         }
+
+//VERIFICO SE MEU NUMERO É MENOR OU MAIOR QUE O NUMERO SECRETO, MEU PROGRAMA ENCERRA QUANDO O NUMERO FOR IGUAL E TENTATIVA FOR=3    
+     }while(chute != numeroAleatorioAte10 && tentativa <3);
+
+     //AQUI EU PEGO O ULTIMO NUMERO QUE EU DIGITEI NA 3º TENTATIVA E COMPARO, SE ELE FOR IGUAL O PROGRAMA FALA QUE EU ACERTEI SE NÃO É POQUE MEU NUMERO DE TENTAIVAS ACABOU
+        if(chute == numeroAleatorioAte10){
+            printf("\nParabens, voce acertou o numero secreto era %d",numeroAleatorioAte10);
+        }
+        else{
+           printf("\nAcabou os numeros de tentativas, voce perdeu! \n O numero secreto era %d",numeroAleatorioAte10);  
+        }
+     //ETAPA 3- Níveis de Dificuldade
+
+            break
+        }
+        case 2:{
+            printf("\n ->NIVEL MEDIO:");
+            int numeroAte50 = (rand() % 50)+1; //7 tentativas
+
+              //ETAPA 2-Número Aleatório e Múltiplas Tentativas
+     //Exibi uma mensagem indicando se o número secreto é maior ou menor do que a tentativa.*/ 
      do{
         printf("\nDigite um numero:");
         scanf("%i",&chute);
@@ -49,6 +91,51 @@ int main(){
         }
      //ETAPA 3- Níveis de Dificuldade
      
+            break
+        }
+        
+        case 3:{
+            printf("\n ->NIVEL DIFICIL:");
+            int numeroAte30 = (rand() % 100)+1; //10 tentativas
+
+              //ETAPA 2-Número Aleatório e Múltiplas Tentativas
+     //Exibi uma mensagem indicando se o número secreto é maior ou menor do que a tentativa.*/ 
+     do{
+        printf("\nDigite um numero:");
+        scanf("%i",&chute);
+        tentativa++;
+
+         if(chute < numeroAleatorioAte10){
+            printf("Tentativa %i: %i \n",tentativa,chute);
+            printf("O numero secreto e maior!\n");
+         }
+
+         else {
+            printf("Tentativa %i: %i \n",tentativa,chute);
+            printf("O numero secreto e menor!\n");
+         }
+
+//VERIFICO SE MEU NUMERO É MENOR OU MAIOR QUE O NUMERO SECRETO, MEU PROGRAMA ENCERRA QUANDO O NUMERO FOR IGUAL E TENTATIVA FOR=3    
+     }while(chute != numeroAleatorioAte10 && tentativa <3);
+
+     //AQUI EU PEGO O ULTIMO NUMERO QUE EU DIGITEI NA 3º TENTATIVA E COMPARO, SE ELE FOR IGUAL O PROGRAMA FALA QUE EU ACERTEI SE NÃO É POQUE MEU NUMERO DE TENTAIVAS ACABOU
+        if(chute == numeroAleatorioAte10){
+            printf("\nParabens, voce acertou o numero secreto era %d",numeroAleatorioAte10);
+        }
+        else{
+           printf("\nAcabou os numeros de tentativas, voce perdeu! \n O numero secreto era %d",numeroAleatorioAte10);  
+        }
+     //ETAPA 3- Níveis de Dificuldade
+           break
+        }
+    }
+
+
+
+
+
+   
+
 
     return 0;
 }
