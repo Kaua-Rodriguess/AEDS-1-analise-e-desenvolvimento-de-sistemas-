d357eb60ce8fa5161f48f4f8f717bca5fa7d3d85
kauã rodrigues <kauarodriges17@gmail.com>
2025-09-14 22:14:52 -0300

    prova antiga AEDS I

---
+++ AEDS I/Aulas 5 e 6 - Estruturas de Repetição/2Periodo/provaAntiga-Q1.c
@@ -0,0 +1,13 @@
+/*!
+13. Números Palíndromos - Peça um número inteiro e verifique se ele é um palíndromo
+(exemplo: 121, 1331) usando um while.
+*/
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+    int
+    float
+    return 0;
+}
+
