e1b1641f0286b72d831c0891d7dea18116b88e1a
kauã rodrigues <kauarodriges17@gmail.com>
2025-10-08 08:26:40 -0300

    Matriz ex1  MATRIZ AEDS

---
+++ AEDS I/Aulas 11 e 12 - Vetores/lara-Listas de Exercícios - Vetores e Matrizes/ex-1.c
@@ -0,0 +1,22 @@
+/*1. Declaração e leitura de vetor: 
+Escreva um programa que leia 10 números inteiros e os armazene em um vetor. 
+Em seguida, imprima os valores lidos. */
+
+#include <stdio.h>
+
+int main(){
+    int vetor[10]; // no meu código eu vou usar o tamanho do vetor e da matriz apenas para condição de parada
+
+    printf("Digite um numero:\n");
+    for(int i=0;i<10;i++){
+        printf("Numero %d:\n",i+1);
+        scanf("%d",&vetor[i]);
+    }
+
+    printf("Posicao:\n");
+    for(int i=0;i<10;i++){
+        printf("[%d]%d\n",i,vetor[i]);
+    }
+
+    return 0;
+}
