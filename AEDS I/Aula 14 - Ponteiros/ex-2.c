6a068e52ad352400b89ad61dfbb1019c832d0432
kauã rodrigues <kauarodriges17@gmail.com>
2025-10-13 16:14:02 -0300

    Atualiza ex-2.c

--- AEDS I/Aula 14 - Ponteiros/ex-2.c
+++ AEDS I/Aula 14 - Ponteiros/ex-2.c
@@ -1,16 +1,23 @@
 //!Atribuição por ponteiro:
 #include <stdio.h>
 #include <stdlib.h>
-void dobro(int *Pnumero){
-    *Pnumero*=2;
-}
+
+void dobro(int *Pnumero);
+
 int main()
 {
     int numero;
-    printf("Digite um numero:");
+    printf("Digite um numero para calcular o dobro:");
     scanf("%d",&numero);
+
     dobro(&numero);
-    printf("Calculando o dobro via ponteiro!\nO dobro e:%d",numero);
+
+    printf("\n O dobro e: %d!",numero);
 
     return 0;
+}
+
+void dobro(int*Pnumero){
+    *Pnumero*=2;
+    //ou *Pnumero=*Pnumero*2;
 }
