980a4b1f149ce22a8bda24330291931651778ec5
kauã rodrigues <kauarodriges17@gmail.com>
2025-10-13 16:15:27 -0300

    Atualiza ex2-salaAula.c

--- AEDS I/Aula 14 - Ponteiros/ex2-salaAula.c
+++ AEDS I/Aula 14 - Ponteiros/ex2-salaAula.c
@@ -5,21 +5,24 @@
 int main(){
     int minhaIdade = 48;
     printf("\nConteudo minhaIdade:%d\n",minhaIdade);
-    printf("Endereco:%p\n",minhaIdade);
+    printf("Endereco:%p\n",&minhaIdade);
 
-    int *PminhaIdade = &minhaIdade;//ponteiro PminhaIdade aponta para o endereço da variável minhaIdade
-    *PminhaIdade;
+    int *ptr = &minhaIdade;//ponteiro PminhaIdade aponta para o endereço da variável minhaIdade
 
-    *PminhaIdade=51;//ponteiro *PminhaIdade está apontando para minhaidade
-                    //o conteudo do endereço do ponteiro *PminhaIdade será passado para o endereço que eu ESTOU APONTANDO 
+    *ptr=51;//ponteiro *PminhaIdade está apontando para minhaidade
+                    //o conteudo do endereço do ponteiro *PminhaIdade será passado para o endereço que eu ESTOU APONTANDO
                     //nesse caso, endereço da variavel minhaIdade
-    printf("\nConteudo *PminhaIdade:%d\n",*PminhaIdade);
-    printf("Endereco:%p\n",*PminhaIdade);
+    printf("\nConteudo ponteiro(*ptr):%d\n",*ptr);
+    printf("Endereco:%p\n",&ptr);
 
-    int*ptr = &*PminhaIdade;//ponteiro apontando pra outro ponteiro qua aponta para uma variavel e mudando valor
-    *ptr=244;
-    printf("\nConteudo *ptr:%d\n",*ptr);
-    printf("Endereco:%p\n",*ptr);
+    printf("Nova idade:%d\n",minhaIdade);
+
+    int*NovoPonteiro = &*ptr;//ponteiro apontando pra outro ponteiro qua aponta para uma variavel e mudando valor
+
+    *NovoPonteiro=244;
+
+    printf("\nConteudo ponteiro(*NovoPonteiro):%d\n",*NovoPonteiro);
+    printf("Endereco:%p\n",&NovoPonteiro);
 
     printf("Nova idade:%d\n",minhaIdade);
 
