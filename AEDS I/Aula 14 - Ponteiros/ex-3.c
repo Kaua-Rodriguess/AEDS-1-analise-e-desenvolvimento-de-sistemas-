2a1adad36dd75aa0ed2c14fc2de472b9aaefed09
kauã rodrigues <kauarodriges17@gmail.com>
2025-10-16 22:29:48 -0300

    executaveis

--- AEDS I/Aula 14 - Ponteiros/ex-3.c
+++ AEDS I/Aula 14 - Ponteiros/ex-3.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 void swap(int*Pn1,int*Pn2){
+    int temp=*Pn1;
     *Pn1=*Pn2;
-    *Pn2=*Pn1;
+    *Pn2=temp;
 }
 
 int main(){
     int n1=5,n2=7;
-    printf("n1=%d n2=%d\n",n1,n2);
+    printf("Numero1=%d Numero2=%d\n",n1,n2);
 
     swap(&n1,&n2);
 
-    printf("n1=%d n2=%d\n",n1,n2);
+    printf("Numero1=%d Numero2=%d\n",n1,n2);
 
     return 0;
 }
--- AEDS I/Aula 14 - Ponteiros/ex-4-explicacao-importante.c
+++ AEDS I/Aula 14 - Ponteiros/ex-4-explicacao-importante.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+void percorreVetor(int *ptr){
+    for(int i=0;i<10;i++){
+        //continuo apontando para o primeiro elemento/casa do vetor,
+        //no entanto estou printando o conteúdo de CADA ELEMENTO/CASA do meu vetor.     
+        printf("\n[%d]:%d",i,*(ptr+i));
+    }
+}
+
 int main(){
     int vetor[10];
 
@@ -9,16 +17,8 @@ int main(){
     }
 
     printf("\nConteudo do vetor percorrendo com ponteiro:");
-    int *ptr=vetor;//aponto para o primeiro elemento/casa do vetor
-    for(int i=0;i<10;i++){
-        printf("\n[%d]:%d",i,*ptr);//imprime o conteudo do primeiro elemento do vetor: vetor[0]
-        ptr++;//aritimética dos ponteiros: ++ eu ando para o proximo elemento/casa do vetor a cada repetição vetor[1],vetor[2]...
-    }
-
+    percorreVetor(vetor);// int *ptr=vetor aponto para o primeiro elemento/casa do vetor
+    
     return 0;
 }
-//OBSERVE QUE O I só serve para imprimir o indice, o ponteiro não precisa dele
-/*LEMBRE-SE:
--Depois do loop, ptr apontará para depois do último elemento do vetor.
--Se tentar usar *ptr fora do loop, pode acessar memória inválida → cuidado.
-*/
+
